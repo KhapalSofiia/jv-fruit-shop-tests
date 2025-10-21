33880ab7e5ffe45ee6c199a5e57b76af07416d0c
sofiia <sofiia.khapal@nure.ua>
2025-10-21 22:15:56 +0200

    bug fixes

--- src/test/java/core/basesyntax/service/impl/ReportExtractorDaoImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportExtractorDaoImplTest.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.service.ReportExtractorDao;
